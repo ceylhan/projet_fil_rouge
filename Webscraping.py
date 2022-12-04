14c24ce139f4870c68a34a3041a4d3e28e71ed1e
Lionel-data <Lionel.galland90@gmail.com>
2022-12-04 09:47:35 +0100

    Correction des bugs et optimisation du temps

--- Webscraping.py
+++ Webscraping.py
@@ -9,7 +9,14 @@ import time
 #Pour rentrer dans chaque page
 increment_page = 4
 
+#clique
+increment_clique = 0
+
 sortir_boucle= 1
+
+#Variable optionnelle en fonction de si on souhaite faire plusieurs fichiers plus petit
+#Permet aussi de limiter les erreurs (temps de réponse)
+modulo_valeur = 170
 
 #On initialise nos listes pour le scrapping
 domaine = []
@@ -51,19 +58,27 @@ while sortir_boucle <= conversion_entier:
 
 
         #On rentre dans le site
-        driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/main/div/div[2]/div/section/div[' + str(increment_page) +']/a/div[2]').click()
-        
+        try:
+            test = driver.find_elements(By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "styles_displayName__GOhL2", " " ))]')
+            test[increment_clique].click()
+            #//*[@id="__next"]/div/div/main/div/div[2]/div/section/div[' + str(increment_page) +']/a/div[2]').click()
+        except:
+            print("Pas de lien pour le site")
+
         #Sleep pour laisser le temps d'afficher la seconde page, sinon erreur car il essaie de scrap sans que la page ne soit complètement affichée
-        time.sleep(3)
+        time.sleep(1)
 
         #Pour les logs
         url = driver.current_url
         print("Traitement du site " + url)
 
         #Titre
-        titre_site = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "title_displayName__TtDDM", " " ))]')
-        titre.append(titre_site.text.strip())
-        #print(titre)
+        try:
+            titre_site = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "title_displayName__TtDDM", " " ))]')
+            titre.append(titre_site.text.strip())
+            #print(titre)
+        except:
+            titre.append('Pas de titre')
 
         #Domaine / catégorie
         try:
@@ -133,27 +148,55 @@ while sortir_boucle <= conversion_entier:
 
 
         increment_page += 1
+        increment_clique +=1
         #Retour sur la page précédente
         driver.back()
 
-    sortir_boucle += 1
-    if sortir_boucle >= 170:
-        print("Toutes les pages ont été parcourus")
-    else:
-        increment_page=4
-        #on clique sur la page suivante
-        driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/main/div/div[2]/div/section/div[24]/nav/a[7]/span').click()
-        #Même chose que pour le précédent sleep
-        time.sleep(3)
+    sortir_boucle += 1  
+    page_reel = sortir_boucle -1
+    increment_clique = 0
 
+    if sortir_boucle >= conversion_entier:
+            Resultat = pd.DataFrame(list(zip(domaine,titre,trustScore,nombreAvis, pourcentage5etoiles,pourcentage4etoiles,pourcentage3etoiles,pourcentage2etoiles,pourcentage1etoiles)), columns=["Catégorie", "Titre","Score","Avis","5 etoiles", "4 etoiles", "3 etoiles", "2 etoiles", "1 etoiles"])
+            Resultat.to_csv('extraction_site_Page_' + str(page_reel) + '_' + str(current_date_time) +'.csv', index = True)
+            print("Toutes les pages ont été parcourus")
+    else:
+        if (((page_reel)%modulo_valeur) == 0):
+            
+            #On créer un fichier toutes les 10 pages traitées au cas ou il y aurait un problème
+            Resultat = pd.DataFrame(list(zip(domaine,titre,trustScore,nombreAvis, pourcentage5etoiles,pourcentage4etoiles,pourcentage3etoiles,pourcentage2etoiles,pourcentage1etoiles)), columns=["Catégorie", "Titre","Score","Avis","5 etoiles", "4 etoiles", "3 etoiles", "2 etoiles", "1 etoiles"])
+            Resultat.to_csv('extraction_site_Page_' + str(page_reel) + '_' + str(current_date_time) +'.csv', index = True)
+
+            increment_page=4
+            #on clique sur la page suivante
+            driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/main/div/div[2]/div/section/div[24]/nav/a[7]/span').click()
+            #Même chose que pour le précédent sleep
+            time.sleep(1)
+
+            #Réinitialisation des listes 
+            domaine = []
+            titre = []
+            trustScore = []
+            nombreAvis = []
+            pourcentage5etoiles = []
+            pourcentage4etoiles = []
+            pourcentage3etoiles = []
+            pourcentage2etoiles = []
+            pourcentage1etoiles = []
+        else:
+            increment_page=4
+            #on clique sur la page suivante
+            driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/main/div/div[2]/div/section/div[24]/nav/a[7]/span').click()
+            #Même chose que pour le précédent sleep
+            time.sleep(1)
 
 
 driver.quit()
 
-Resultat = pd.DataFrame(list(zip(domaine,titre,trustScore,nombre_avis, pourcentage5etoiles,pourcentage4etoiles,pourcentage3etoiles,pourcentage2etoiles,pourcentage1etoiles)), columns=["Catégorie", "Titre","Score","Avis","5 etoiles", "4 etoiles", "3 etoiles", "2 etoiles", "1 etoiles"])
+Resultat = pd.DataFrame(list(zip(domaine,titre,trustScore,nombreAvis, pourcentage5etoiles,pourcentage4etoiles,pourcentage3etoiles,pourcentage2etoiles,pourcentage1etoiles)), columns=["Catégorie", "Titre","Score","Avis","5 etoiles", "4 etoiles", "3 etoiles", "2 etoiles", "1 etoiles"])
 
 #Extraction en csv avec la date
-Resultat.to_csv('extraction_site_'+ str(current_date_time) +'.csv', index = True)
+Resultat.to_csv('extraction_site_Page_' + str(page_reel) + '_' + str(current_date_time) +'.csv', index = True)
 
 
 
