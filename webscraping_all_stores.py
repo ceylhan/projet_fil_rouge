ee9139a84e28dbcc70ac8bdd4f10c21721fae41f
ceylhan <37987261+ceylhan@users.noreply.github.com>
2022-12-18 19:17:06 +0100

    Add files via upload
    Add webscraping code and csv file

---
+++ webscraping_all_stores.py
@@ -0,0 +1,9 @@
+# Librairies
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
+from FileImport import write_csv
+from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
