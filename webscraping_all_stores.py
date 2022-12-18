3464e39338cfe39836c4d9e43572ca6fd0a35ef2
ceylhan <37987261+ceylhan@users.noreply.github.com>
2022-12-18 20:52:56 +0100

    delete

--- webscraping_all_stores.py
+++
@@ -1,9 +0,0 @@
-# Librairies
-from selenium import webdriver
-import pandas as pd
-from selenium.webdriver.common.by import By
-from FileImport import write_csv
-from datetime import datetime
-from selenium.webdriver.common.action_chains import ActionChains
-import time
-
